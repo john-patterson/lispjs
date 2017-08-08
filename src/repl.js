135de6347296225557454b488e7c6b93dfecd019
John Patterson <john@JohnPPatterson.com>
2017-08-07 22:15:14 -0700

    marks REPL as main entry point

---
+++ src/repl.js
@@ -0,0 +1,31 @@
+const { Scanner } = require('../src/scanner');
+const { Parser } = require('../src/parser');
+const { Interpreter } = require('../src/interpreter');
+const { Env } = require('../src/env');
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let util = require('util');
+
+let interpreter = new Interpreter();
+let parser = new Parser();
+let scanner = new Scanner();
+let env = Env.standard();
+
+let isDebug = false;
+let debug = (message) => {
+    if (isDebug) {
+        console.log(message);
+    }
+}
+
+process.stdin.on('data', (text) => {
+    debug(`Tokenizing input: ${text}`);
+    let tokenStream = scanner.tokenize(text);
+    debug(`Parsing tokenStream: ${JSON.stringify(tokenStream)}`);
+    let ast = parser.parse(tokenStream);
+    debug(`Running AST: ${JSON.stringify(ast)}`);
+    let result = interpreter.run(ast, env);
+    console.log(`=> ${result}`);
+});
